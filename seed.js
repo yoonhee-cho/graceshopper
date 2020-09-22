4ece79c4f17af6dbd9b6e355057b68ab3b734612
Hedra Rowan <hedrarowan@gmail.com>
2020-09-22 10:47:59 -0500

    seed file initialized

---
+++ seed.js
@@ -0,0 +1,16 @@
+const { db, Book, Author } = require('./server/db')
+
+const seed = async () => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = seed;
+
+if(require.main === module) {
+    seed()
+    .then
+}
