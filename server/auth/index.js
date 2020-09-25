48e47f386a729670cd02a4651a5e34d4ba74d519
Melody <mcmelodychen@gmail.com>
2020-09-25 17:39:22 -0400

    Google OAth WIP

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
+const {Order, Book} = require('../db/models')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
   try {
-    const user = await User.findOne({where: {email: req.body.email}})
+    // added Order model for this Google user(if user exists in our db)
+    console.log('HELLLLO req.body: ', req.body)
+
+    const user = await User.findOne({
+      where: {email: req.body.email},
+      include: [{model: Order}]
+    })
     if (!user) {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
@@ -21,7 +28,16 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    let user = await User.create(req.body)
+    // create a new user
+    const order = await Order.create({
+      userId: user.id
+    })
+    user = await User.findByPk(user.id, {
+      include: {
+        model: Order
+      }
+    })
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
@@ -38,8 +54,19 @@ router.post('/logout', (req, res) => {
   res.redirect('/')
 })
 
-router.get('/me', (req, res) => {
-  res.json(req.user)
+router.get('/me', async (req, res) => {
+  try {
+    if (!req.user) {
+      res.json(req.user)
+    } else {
+      const user = await User.findByPk(req.user.id, {
+        include: [{model: Order}]
+      })
+      res.json(user)
+    }
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 router.use('/google', require('./google'))
