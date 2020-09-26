e28bbc785fa12da8b7c17026250eed98d241e5b0
Kathryn Lam <katwingki@gmail.com>
2020-09-26 00:46:17 -0400

    TESTING for HEROKU to see if both the localhost & the heroku would work

--- server/auth/google.js
+++ server/auth/google.js
@@ -24,7 +24,8 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+    callbackURL:
+      process.env.GOOGLE_CALLBACK || process.env.GOOGLE_HEROKU_CALLBACK
   }
 
   const strategy = new GoogleStrategy(
